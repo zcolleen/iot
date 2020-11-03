7a8e3a0fb8e7a878fbda163c6cd98fcc8ff5e422
zcolleen <semeneigin@yandex.ru>
2020-11-04 02:43:56 +0300

    trigger done

--- reciever.sh
+++ reciever.sh
@@ -1,6 +1,6 @@
 yc iot mqtt subscribe \
 --debug \
---cert keys_certs/cert_storage.pem \
---key keys_certs/key_storage.pem \
---topic '$devices/are18v6krffaq7o1mldk/commands' \
+--cert keys_certs/cert_conveyer.pem \
+--key keys_certs/key_conveyer.pem \
+--topic '$devices/are6c1grj2ojp532jr3u/commands' \
 --qos 1
